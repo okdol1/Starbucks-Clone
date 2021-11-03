3fc5fea466657ebec2331966b4ffeb92ce8c1434
okdol1 <okdol0611@gmail.com>
2021-11-03 15:13:26 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(function () {
@@ -141,6 +124,3 @@ spyEls.forEach(function(spyEl) {
 });
 
 
-
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear() //2021
